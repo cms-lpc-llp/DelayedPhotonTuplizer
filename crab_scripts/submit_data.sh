6be5fc6264bc2d36eeb0af9e8f1059679cec60f2
Thong <thongnguyen1292@gmail.com>
2019-12-12 23:56:03 +0100

    Modify submit script

--- crab_scripts/submit_data.sh
+++ crab_scripts/submit_data.sh
@@ -3,35 +3,36 @@ then
     mkdir submit
 fi
 
-for lambda in 100 150 200 250 300 350 400
+for PS in 'SinglePhoton' 'DoubleEG'
 do
-        for ctau in 10 200 400 600 800 1000 1200
+        for era in B C D E F
         do
-                subfile=submit/GMSB_L-${lambda}TeV_Ctau-${ctau}cm_central.py
-                query_="dasgoclient --query=\"dataset=/GMSB_L-${lambda}TeV*Ctau-${ctau}cm*/RunIIFall17MiniAODv2*/* instance=prod/global\""
+                subfile=submit/${PS}_Run2017${era}.py
+                query_="dasgoclient --query=\"dataset=/${PS}/Run2017${era}-31Mar2018-v1/MINIAOD* instance=prod/global\""
                 inputDataset=`eval ${query_}`
                 echo ${inputDataset}
                 echo "from WMCore.Configuration import Configuration" > ${subfile}
                 echo "config = Configuration()" >> ${subfile}
                 echo "" >> ${subfile}
                 echo "config.section_(\"General\")" >> ${subfile}
-                echo "config.General.requestName = 'prod_Run2DelayedPhotonNtupler_MC2017_GMSB_L${lambda}TeV_Ctau-${ctau}cm_central'" >> ${subfile} 
+                echo "config.General.requestName = 'prod_Run2DelayedPhotonNtupler_Data2017_${PS}_Run2017${era}'" >> ${subfile} 
                 echo "config.General.workArea = 'crab'" >> ${subfile} 
                 echo "" >> ${subfile} 
                 echo "config.section_(\"JobType\")" >> ${subfile} 
                 echo "config.JobType.pluginName = 'Analysis'" >> ${subfile} 
-                echo "config.JobType.psetName = '${CMSSW_BASE}/src/Tuplizer/DelayedPhotonTuplizer/python/razorTuplizer_MC_Fall17_EcalRechits_reMiniAOD_OOT.py'" >> ${subfile} 
+                echo "config.JobType.psetName = '${CMSSW_BASE}/src/Tuplizer/DelayedPhotonTuplizer/python/razorTuplizer_Data_2017_Rereco_EcalRechits_OOTpho.py'" >> ${subfile} 
                 echo "config.JobType.allowUndistributedCMSSW = True" >> ${subfile} 
                 echo "" >> ${subfile} 
                 echo "config.section_(\"Data\")" >> ${subfile} 
                 echo "config.Data.publication = False" >> ${subfile} 
                 echo "config.Data.inputDataset = '${inputDataset}'" >> ${subfile} 
-                echo "config.Data.splitting = 'FileBased'" >> ${subfile} 
-                echo "config.Data.unitsPerJob = 1" >> ${subfile} 
+                echo "config.Data.splitting = 'LumiBased'" >> ${subfile} 
+                echo "config.Data.unitsPerJob = 100" >> ${subfile} 
+                echo "config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'" >> ${subfile} 
                 echo "" >> ${subfile} 
                 echo "config.section_(\"Site\")" >> ${subfile} 
                 echo "config.Site.storageSite = 'T2_US_Caltech'" >> ${subfile} 
-                echo "config.Data.outLFNDirBase = '/store/group/phys_susy/razor/run2/Run2DelayedPhotonNtuple/MC/MC2017CentralGMSB/'" >> ${subfile} 
+                echo "config.Data.outLFNDirBase = '/store/group/phys_susy/razor/run2/Run2DelayedPhotonNtuple/Data2017/'" >> ${subfile} 
                 crab submit -c ${subfile} 
         done
 done
