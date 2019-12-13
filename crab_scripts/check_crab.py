7c18c9a8a25dbb382a3181cb035eb001e9c34253
Thong <thongnguyen1292@gmail.com>
2019-12-13 03:44:02 +0100

    Add resubmit option

--- crab_scripts/check_crab.py
+++ crab_scripts/check_crab.py
@@ -5,7 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s","--sample", help='Sample to check')
-parser.add_argument("-r","--resubmit", action='store_true', default=False, help='Whether to resubmit failed jobs')
+parser.add_argument("-r","--resubmit", action='store_true', default=False, help='Whether to resubmit failed jobs even if task is not finished')
 args = parser.parse_args()
 
 to_check = args.sample
@@ -17,18 +17,18 @@ for task in tasks:
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     lines = stdout.split('\n')
-    isComplete = False
+    isFailed = False
     endPrint = None
     for i, line in enumerate(lines):
         if "Status on the scheduler" in line:
-            if 'COMPLETED' in line: 
-                isComplete = True
+            if 'FAILED' in line: 
+                isFailed = True
             beginPrint = i
         if "transferring" in line:
             endPrint = i+1
     if not endPrint: endPrint = beginPrint + 5
     print("\n".join(lines[beginPrint:endPrint]))
-    if args.resubmit: 
+    if args.resubmit or isFailed: 
         cmd = 'crab resubmit -d {}'.format(task)
         print(cmd)
         subprocess.call(cmd, shell=True)
