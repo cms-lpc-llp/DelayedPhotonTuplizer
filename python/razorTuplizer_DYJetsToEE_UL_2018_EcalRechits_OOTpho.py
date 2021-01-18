08b8ba18db43cd9d388735204271293e3675e30b
YeongDeok Seo <yeseo@cern.ch>
2021-01-18 08:13:06 -0800

    update code for MC

--- python/razorTuplizer_DYJetsToEE_UL_2018_EcalRechits_OOTpho.py
+++ python/razorTuplizer_DYJetsToEE_UL_2018_EcalRechits_OOTpho.py
@@ -19,18 +19,18 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 #TFileService for output 
 process.TFileService = cms.Service("TFileService", 
-    fileName = cms.string("razorNtuple_DYToEE_2018UL.root"),
-    closeFileFast = cms.untracked.bool(True),
+    fileName = cms.string("razorNtuple.root"),
+    closeFileFast = cms.untracked.bool(True)
 )
 
 #load run conditions
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.Geometry.GeometryIdeal_cff')
-process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 
 #------ Declare the correct global tag ------#
 
-# https://twiki.cern.ch/twiki/bin/viewauth/CMS/PdmVAnalysisSummaryTable
+#Global Tag for Run2015D
 process.GlobalTag.globaltag = '106X_upgrade2018_realistic_v15_L1v1'
 
 #------ If we add any inputs beyond standard miniAOD event content, import them here ------#
@@ -40,15 +40,16 @@ process.GlobalTag.globaltag = '106X_upgrade2018_realistic_v15_L1v1'
 #process.HBHENoiseFilterResultProducer.IgnoreTS4TS5ifJetInLowBVRegion=cms.bool(False) 
 #process.HBHENoiseFilterResultProducer.defaultDecision = cms.string("HBHENoiseFilterResultRun2Loose")
 
-process.load('RecoMET.METFilters.BadChargedCandidateFilter_cfi')
-process.BadChargedCandidateFilter.muons = cms.InputTag("slimmedMuons")
-process.BadChargedCandidateFilter.PFCandidates = cms.InputTag("packedPFCandidates")
-process.BadChargedCandidateFilter.taggingMode = cms.bool(True)
+#process.load('RecoMET.METFilters.BadChargedCandidateFilter_cfi')
+#process.BadChargedCandidateFilter.muons = cms.InputTag("slimmedMuons")
+#process.BadChargedCandidateFilter.PFCandidates = cms.InputTag("packedPFCandidates")
+#process.BadChargedCandidateFilter.taggingMode = cms.bool(True)
 
 process.load('RecoMET.METFilters.BadPFMuonFilter_cfi')
 process.BadPFMuonFilter.muons = cms.InputTag("slimmedMuons")
 process.BadPFMuonFilter.PFCandidates = cms.InputTag("packedPFCandidates")
 process.BadPFMuonFilter.taggingMode = cms.bool(True)
+
 
 #------ Electron MVA Setup ------#
 # define which IDs we want to produce
@@ -62,61 +63,71 @@ for idmod in my_id_modules:
     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
 
+
 #------ Analyzer ------#
 from PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi import unpackedTracksAndVertices
 process.unpackedTracksAndVertices = unpackedTracksAndVertices.clone()
 
 #list input collections
 process.ntuples = cms.EDAnalyzer('RazorTuplizer', 
-    isData = cms.bool(True),    
-    useGen = cms.bool(False),
+    isData = cms.bool(False),    
+    useGen = cms.bool(True),
     isFastsim = cms.bool(False),
     enableTriggerInfo = cms.bool(True),                                 
-    enableEcalRechits = cms.bool(True),                    
-    enableAK8Jets = cms.bool(False), 
-    triggerPathNamesFile = cms.string("Tuplizer/DelayedPhotonTuplizer/data/RazorHLTPathnames2018.dat"),
-    eleHLTFilterNamesFile = cms.string("Tuplizer/DelayedPhotonTuplizer/data/RazorElectronHLTFilterNames2018.dat"),
-    muonHLTFilterNamesFile = cms.string("Tuplizer/DelayedPhotonTuplizer/data/RazorMuonHLTFilterNames2018.dat"),
-    photonHLTFilterNamesFile = cms.string("Tuplizer/DelayedPhotonTuplizer/data/RazorPhotonHLTFilterNames2018.dat"),
+    enableEcalRechits = cms.bool(True),                                 
+    readGenVertexTime = cms.untracked.bool(True),
+    genParticles_t0 = cms.InputTag("genParticles", "t0", ""), 
+    triggerPathNamesFile = cms.string("Tuplizer/DelayedPhotonTuplizer/data/RazorHLTPathnames2017.dat"),
+    eleHLTFilterNamesFile = cms.string("Tuplizer/DelayedPhotonTuplizer/data/RazorElectronHLTFilterNames2017.dat"),
+    muonHLTFilterNamesFile = cms.string("Tuplizer/DelayedPhotonTuplizer/data/RazorMuonHLTFilterNames2017.dat"),
+    photonHLTFilterNamesFile = cms.string("Tuplizer/DelayedPhotonTuplizer/data/RazorPhotonHLTFilterNames2017.dat"),
 
     vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
     tracks = cms.InputTag("unpackedTracksAndVertices"),
+    
     muons = cms.InputTag("slimmedMuons"),
     electrons = cms.InputTag("slimmedElectrons"),
     taus = cms.InputTag("slimmedTaus"),
-    photons = cms.VInputTag(cms.InputTag("slimmedPhotons"), cms.InputTag("slimmedOOTPhotons")), #standard photon first, OOT and other photons follow next
+    photons = cms.VInputTag(cms.InputTag("slimmedPhotons"),cms.InputTag("slimmedOOTPhotons")), # please put the standard photon collection to the first one
     jets = cms.InputTag("slimmedJets"),
     jetsPuppi = cms.InputTag("slimmedJetsPuppi"),
     jetsAK8 = cms.InputTag("slimmedJetsAK8"),
-    puppiSDjetLabel = cms.InputTag('packedPatJetsAK8PFPuppiSoftDrop'),
+    enableAK8Jets = cms.bool(False),
     jetsAK8SoftDropPacked = cms.InputTag("selectedPatJetsAK8PFCHSSoftDropPacked"),
     jetsAK8Subjets = cms.InputTag("selectedPatJetsAK8PFCHSSoftDropPacked", "SubJets"),
+    puppiSDjetLabel = cms.InputTag('packedPatJetsAK8PFPuppiSoftDrop'),
     mets = cms.InputTag("slimmedMETs"),
+    metsEGClean = cms.InputTag("slimmedMETsEGClean","","PAT"),
+    metsMuEGClean = cms.InputTag("slimmedMETsMuEGClean","","PAT"),
+    metsMuEGCleanCorr = cms.InputTag("slimmedMETsMuEGClean","","razorTuplizer"),
+    metsUncorrected = cms.InputTag("slimmedMETsUncorrected"),
     metsNoHF = cms.InputTag("slimmedMETsNoHF"),
     metsPuppi = cms.InputTag("slimmedMETsPuppi"),
     packedPfCands = cms.InputTag("packedPFCandidates"),
 
     packedGenParticles = cms.InputTag("packedGenParticles"),
     prunedGenParticles = cms.InputTag("prunedGenParticles"),
-    genJets = cms.InputTag("slimmedGenJets", "", "RECO"),
+    genJets = cms.InputTag("slimmedGenJets", "", "PAT"),
 
     triggerBits = cms.InputTag("TriggerResults","","HLT"),
     triggerPrescales = cms.InputTag("patTrigger"),
     triggerObjects = cms.InputTag("slimmedPatTrigger"),
-    metFilterBits = cms.InputTag("TriggerResults", "", "RECO"),
+    metFilterBits = cms.InputTag("TriggerResults", "", "PAT"),
     hbheNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHENoiseFilterResult"),
     hbheTightNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHENoiseFilterResultRun2Tight"),
     hbheIsoNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHEIsoNoiseFilterResult"),
-    BadChargedCandidateFilter = cms.InputTag("BadChargedCandidateFilter",""),
+    #BadChargedCandidateFilter = cms.InputTag("BadChargedCandidateFilter",""),
     BadMuonFilter = cms.InputTag("BadPFMuonFilter",""),
+    badGlobalMuonFilter = cms.InputTag("badGlobalMuonTagger","bad"),
+    duplicateMuonFilter = cms.InputTag("cloneGlobalMuonTagger","bad"),
 
-    lheInfo = cms.InputTag("externalLHEProducer", "", "LHE"),
+    lheInfo = cms.InputTag("externalLHEProducer", "", ""),
     genInfo = cms.InputTag("generator", "", "SIM"),
-    puInfo = cms.InputTag("addPileupInfo", "", "HLT"), #uncomment if no pre-mixing
+    puInfo = cms.InputTag("slimmedAddPileupInfo", "", "PAT"), #uncomment if no pre-mixing
     #puInfo = cms.InputTag("mixData", "", "HLT"), #uncomment for samples with pre-mixed pileup
-    hcalNoiseInfo = cms.InputTag("hcalnoise", "", "RECO"),
+    hcalNoiseInfo = cms.InputTag("hcalnoise", "", "PAT"),
 
-    secondaryVertices = cms.InputTag("slimmedSecondaryVertices", "", "RECO"),
+    secondaryVertices = cms.InputTag("slimmedSecondaryVertices", "", "PAT"),
 
     rhoAll = cms.InputTag("fixedGridRhoAll", "", "RECO"),
     rhoFastjetAll = cms.InputTag("fixedGridRhoFastjetAll", "", "RECO"),
@@ -127,18 +138,22 @@ process.ntuples = cms.EDAnalyzer('RazorTuplizer',
 
     beamSpot = cms.InputTag("offlineBeamSpot", "", "RECO"),
 
-    ebRecHits = cms.InputTag("reducedEgamma", "reducedEBRecHits", "RECO"),
-    eeRecHits = cms.InputTag("reducedEgamma", "reducedEERecHits", "RECO"),
-    esRecHits = cms.InputTag("reducedEgamma", "reducedESRecHits", "RECO"),
-    ebeeClusters = cms.InputTag("reducedEgamma", "reducedEBEEClusters", "RECO"),
-    esClusters = cms.InputTag("reducedEgamma", "reducedESClusters", "RECO"),
-    conversions = cms.InputTag("reducedEgamma", "reducedConversions", "RECO"),
-    singleLegConversions = cms.InputTag("reducedEgamma", "reducedSingleLegConversions", "RECO"),
-    gedGsfElectronCores = cms.InputTag("reducedEgamma", "reducedGedGsfElectronCores", "RECO"),
-    gedPhotonCores = cms.InputTag("reducedEgamma", "reducedGedPhotonCores", "RECO"),
-    superClusters = cms.InputTag("reducedEgamma", "reducedSuperClusters", "RECO"),
+    ebRecHits = cms.InputTag("reducedEgamma", "reducedEBRecHits", "PAT"), 
+    eeRecHits = cms.InputTag("reducedEgamma", "reducedEERecHits", "PAT"),
+    esRecHits = cms.InputTag("reducedEgamma", "reducedESRecHits", "PAT"),
+    ebeeClusters = cms.InputTag("reducedEgamma", "reducedEBEEClusters", "PAT"),
+    #ebeeClusters = cms.VInputTag(cms.InputTag("reducedEgamma", "reducedEBEEClusters", "PAT"), cms.InputTag("reducedEgamma", "reducedOOTEBEEClusters", "PAT")),
+    esClusters = cms.InputTag("reducedEgamma", "reducedESClusters", "PAT"),
+    #esClusters = cms.VInputTag(cms.InputTag("reducedEgamma", "reducedESClusters", "PAT"), cms.InputTag("reducedEgamma", "reducedOOTESClusters", "PAT")),
+    conversions = cms.InputTag("reducedEgamma", "reducedConversions", "PAT"),
+    singleLegConversions = cms.InputTag("reducedEgamma", "reducedSingleLegConversions", "PAT"),
+    gedGsfElectronCores = cms.InputTag("reducedEgamma", "reducedGedGsfElectronCores", "PAT"),
+    gedPhotonCores = cms.InputTag("reducedEgamma", "reducedGedPhotonCores", "PAT"),
+    #gedPhotonCores = cms.VInputTag(cms.InputTag("reducedEgamma", "reducedGedPhotonCores", "PAT"), cms.InputTag("reducedEgamma", "reducedOOTPhotonCores", "PAT")),
+    superClusters = cms.InputTag("reducedEgamma", "reducedSuperClusters", "PAT"),
+    #superClusters = cms.VInputTag(cms.InputTag("reducedEgamma", "reducedSuperClusters", "PAT"), cms.InputTag("reducedEgamma", "reducedOOTSuperClusters", "PAT")),
 
-    lostTracks = cms.InputTag("lostTracks", "", "RECO"),
+    lostTracks = cms.InputTag("lostTracks", "", "PAT"),
     mvaGeneralPurposeValuesMap     = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16GeneralPurposeV1Values"),
     mvaGeneralPurposeCategoriesMap = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16GeneralPurposeV1Categories"),
     mvaHZZValuesMap     = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16HZZV1Values"),
@@ -147,7 +162,8 @@ process.ntuples = cms.EDAnalyzer('RazorTuplizer',
 
 #run
 process.p = cms.Path( process.egmGsfElectronIDSequence *
-                      process.BadChargedCandidateFilter*
+                      #process.HBHENoiseFilterResultProducer*
+                      #process.BadChargedCandidateFilter*
                       process.BadPFMuonFilter*
                       process.unpackedTracksAndVertices *
                       process.ntuples)
